2a3d13017f17d4d4cef9d8ac13a52b892713b4a3
ali <ali599115225@gmail.com>
2026-06-20 11:18:50 +0300

    fix(security): allow oauth popup communication

--- next.config.mjs
+++ next.config.mjs
@@ -63,7 +63,7 @@ const nextConfig = {
           },
           {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
+            value: 'same-origin-allow-popups',
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
